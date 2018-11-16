b12e6a587221c4c1acd75bace5d105a50d84304f
rafaelno0 <rr8342@gmail.com>
2018-11-16 11:10:06 -0200

    Arrow Functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,16 @@
-let o = {
-  carId: 123,
-  getId: function() {
-    return this.carId;
-  }
+//Arrow function do not have their own "this" value.
+// "this" refers to the enclosing context
+
+let getId = (prefix, suffix) => {
+  return prefix + 123 + suffix;
 };
 
-let newCar = { carId: 456};
+console.log(getId('ID: ', '!'));
 
-let newFn = o.getId.bind(newCar);
+let getId2 = (prefix, suffix) => prefix + 123 + suffix;
 
-console.log(newFn());
+console.log(getId2('ID: ', '!'));
+
+let getId3 = _ => 123; //You can change if you want () to _ is a convention
+
+console.log(getId3());
